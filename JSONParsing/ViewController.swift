7ed211ff3c7948660511cff9a0e405ba611cb35b
“RajeeSri” <rajsri.j@gmail.com>
2020-03-16 11:29:39 -0400

    Initial Commit

---
+++ JSONParsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParsing
+//
+//  Created by YaathmiAR on 3/16/20.
+//  Copyright © 2020 YaathmiAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
