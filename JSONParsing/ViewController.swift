6a816511990f0261e2961d32b25ada642621cf3c
“RajeeSri” <rajsri.j@gmail.com>
2020-03-16 20:06:50 -0400

    Initial upload

--- JSONParsing/ViewController.swift
+++ JSONParsing/ViewController.swift
@@ -10,16 +10,129 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movieTableView: UITableView!
+    var movieList : [Movie] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadJSONData()
     }
+    
+    func loadJSONData(){
+        
+        
+        guard let url = Bundle.main.url(forResource: "MarvelMovies", withExtension:"json")else{
+            
+            print("File not found")
+            return;
+        }
+        
+        do{
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            guard let jsonObj = json,let moviesDict = jsonObj["Marvel Cinematic Universe"] as? [String : Any]  else {
+                return
+            }
+            
+            
+            
+            for (_,value) in moviesDict{
+                
+                if let movie = value as? [String:Any]{
+                    
+                    if let movieObj = Movie(json: movie){
+                        
+                        movieList.append(movieObj)
+                    }
+                }
+                
+            }
+            
+            movieTableView.reloadData()
+            
+            
+        }
+        catch{
+            
+            print(error.localizedDescription)
+        }
+        
+        
     }
-
+    
 
 }
+extension ViewController : UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        let movie = movieList[indexPath.row]
+        tableViewCell.textLabel?.text = movie.title
+        tableViewCell.detailTextLabel?.text = movie.category
+        
+        
+        return tableViewCell
+        
+    }
+    
+    
+}
 
+struct Movie {
+    let title : String
+    let category : String
+    let phase : String
+    let movieID : Int
+    let releaseYear : Int
+    
+    init?(json : Dictionary<String, Any>){
+        
+        
+        
+        guard let title = json["title"] as? String , let category = json["category_name"] as? String, let phase = json["phase"] as? String , let movieID = json["movie_id"] as? Int, let releaseYear = json["release_year"] as? Int  else {
+            return nil
+        }
+        self.title = title
+        self.category = category
+        self.phase = phase
+        self.movieID = movieID
+        self.releaseYear = releaseYear
+        
+        
+        //self.title = json["title"] as? String ?? ""
+        //self.movieID = json["movie_id"] as? Int ?? 0
+
+    }
+    
+    
+}
+
+/*
+ 
+ "movie_id" : 1,
+ "title" : "Iron Man",
+ "phase" : "Phase One: Avengers Assembled",
+ "category_name" : "Action",
+ "release_year" : 2015,
+ "running_time" : 126,
+ "rating_name" : "PG-13",
+ "disc_format_name" : "Blu-ray",
+ "number_discs" : 1,
+ "viewing_format_name" : "Widescreen",
+ "aspect_ratio_name" : "2.35:1",
+ "status" : 1,
+ "release_date" : "May 2, 2008",
+ "budget" : "140,000,000",
+ "gross" : "318,298,180",
+ "time_stamp" : "2015-05-03"
+
+ 
+ */
